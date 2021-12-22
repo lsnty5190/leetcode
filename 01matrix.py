977e6b9a2939bf9b8bf2f936672c48a9a22ee977
lsnty5190 <1758213469@qq.com>
2021-12-22 23:07:48 +0800

    update code

--- 01matrix.py
+++ 01matrix.py
@@ -14,11 +14,14 @@ for i in range(len(mat)):
             queue.append((i,j))
             visited.add((i,j))
 
+# BFS with queue
 while queue:
     x, y = queue.popleft()
     for ord in [(1,0), (-1,0), (0,1), (0,-1)]:
         newx, newy = x+ord[0], y+ord[1]
+        # bound
         if newx >=0 and newx <= len(mat)-1 and newy >=0 and newy <= len(mat[0])-1 and (newx,newy) not in visited:
+            # add a step
             mat[newx][newy] = mat[x][y] + 1
             queue.append((newx,newy))
             visited.add((newx,newy))
